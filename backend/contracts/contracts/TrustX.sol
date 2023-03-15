d3b3ed34e4272e213f3d1a32db5bf152202ed69c
kourin <kourin.code@gmail.com>
2023-03-15 19:18:27 +0900

    Implement interface of contract

--- backend/contracts/contracts/TrustX.sol
+++ backend/contracts/contracts/TrustX.sol
@@ -17,11 +17,41 @@ contract TrustX is Initializable, PausableUpgradeable {
     string constant ERR_DAO_NOT_FOUND = "ERR_DAO_NOT_FOUND";
 
     // enums
+    enum TaskStatus {
+        TODO,
+        InProgress,
+        InReview,
+        Done
+    }
+
+    enum TaskThreadType {
+        Comment,
+        ReviewRequest,
+        ChangesRequest,
+        Approve
+    }
+
     // structs
     struct DAO {
         address createdBy;
         string name;
         string metadataURI;
+        bool isPrivate;
+    }
+
+    struct Task {
+        address createdBy;
+        TaskStatus status;
+        string name;
+        string metadataURI;
+        bool isPrivate;
+        string[] skills;
+    }
+
+    struct TaskThread {
+        address createdBy;
+        TaskThreadType threadType;
+        string messageURI;
         bool isPrivate;
     }
 
@@ -31,6 +61,15 @@ contract TrustX is Initializable, PausableUpgradeable {
     mapping(uint256 => DAO) private _daos; // DAO ID => DAO
 
     mapping(uint256 => EnumerableSet.AddressSet) private _daoMembers; // DAO ID => Address Set
+
+    mapping(uint256 => Counters.Counter) private _taskIDs; // DAO ID => Counter
+
+    mapping(uint256 => mapping(uint256 => Task)) private _tasks; // DAO ID => Task ID => Task
+
+    mapping(uint256 => mapping(uint256 => Counters.Counter)) private _threadIDs; // DAO ID => Task ID => Counter
+
+    mapping(uint256 => mapping(uint256 => mapping(uint256 => TaskThread)))
+        private _taskThreads; // DAO ID => Task ID => Thread ID => TaskThread
 
     // Events
     event DAOCreated(
@@ -42,9 +81,65 @@ contract TrustX is Initializable, PausableUpgradeable {
 
     event DAOMembersAdded(uint256 indexed daoID, address[] members);
 
+    event TaskCreated(
+        uint256 indexed daoID,
+        uint256 taskID,
+        string name,
+        string metadataURI,
+        bool isPrivate,
+        string[] skills
+    );
+
+    event AssignerAssigned(
+        uint256 indexed daoID,
+        uint256 indexed taskID,
+        address assigner
+    );
+
+    event ReviewersAssigned(
+        uint256 indexed daoID,
+        uint256 indexed taskID,
+        address[] reviewers
+    );
+
+    event TaskStarted(uint256 indexed daoID, uint256 indexed taskID);
+
+    event ReviewRequested(
+        uint256 indexed daoID,
+        uint256 indexed taskID,
+        uint256 threadID,
+        string messageURI,
+        bool isPrivate
+    );
+
+    event RequestedChanges(
+        uint256 indexed daoID,
+        uint256 indexed taskID,
+        uint256 threadID,
+        address requestedBy,
+        string messageURI,
+        bool isPrivate
+    );
+
+    event TaskApproved(
+        uint256 indexed daoID,
+        uint256 indexed taskID,
+        address approvedBy,
+        string messageURI,
+        bool isPrivate,
+        uint256[] scores
+    );
+
+    event TaskCompleted(uint256 indexed daoID, uint256 indexed taskID);
+
     // Modifiers
     modifier daoExists(uint256 daoID) {
         require(_daos[daoID].createdBy != address(0x0), ERR_DAO_NOT_FOUND);
+        _;
+    }
+
+    modifier taskExists(uint256 daoID, uint256 taskID) {
+        // TODO: implement
         _;
     }
 
@@ -70,7 +165,136 @@ contract TrustX is Initializable, PausableUpgradeable {
         return _addMembers(daoID, newMembers);
     }
 
+    function creataTask(
+        uint256 daoID,
+        string memory name,
+        string memory metadataURI,
+        bool isPrivate,
+        string[] memory skills,
+        address assigner,
+        address[] memory reviewers
+    ) external whenNotPaused daoExists(daoID) {
+        _creataTask(
+            daoID,
+            name,
+            metadataURI,
+            isPrivate,
+            skills,
+            assigner,
+            reviewers
+        );
+    }
+
+    function assignAssigner(
+        uint256 daoID,
+        uint256 taskID,
+        address assigner
+    ) external whenNotPaused daoExists(daoID) taskExists(daoID, taskID) {
+        _assignAssigner(daoID, taskID, assigner);
+    }
+
+    function assignReviewers(
+        uint256 daoID,
+        uint256 taskID,
+        address[] memory reviewers
+    ) external whenNotPaused daoExists(daoID) taskExists(daoID, taskID) {
+        _assignReviewers(daoID, taskID, reviewers);
+    }
+
+    function startTask(
+        uint256 daoID,
+        uint256 taskID
+    ) external whenNotPaused daoExists(daoID) taskExists(daoID, taskID) {
+        _startTask(daoID, taskID);
+    }
+
+    function addComment(
+        uint256 daoID,
+        uint256 taskID,
+        string memory messageURI,
+        bool isPrivate
+    ) external whenNotPaused daoExists(daoID) taskExists(daoID, taskID) {
+        _addComment(daoID, taskID, messageURI, isPrivate);
+    }
+
+    function requestTaskReview(
+        uint256 daoID,
+        uint256 taskID,
+        string memory messageURI,
+        bool isPrivate
+    ) external whenNotPaused daoExists(daoID) taskExists(daoID, taskID) {
+        _requestTaskReview(daoID, taskID, messageURI, isPrivate);
+    }
+
+    function requestChanges(
+        uint256 daoID,
+        uint256 taskID,
+        string memory messageURI,
+        bool isPrivate
+    ) external whenNotPaused daoExists(daoID) taskExists(daoID, taskID) {
+        _requestChanges(daoID, taskID, messageURI, isPrivate);
+    }
+
+    function approveTask(
+        uint256 daoID,
+        uint256 taskID,
+        string memory messageURI,
+        bool isPrivate,
+        uint256[] memory scores
+    ) external whenNotPaused daoExists(daoID) taskExists(daoID, taskID) {
+        _approveTask(daoID, taskID, messageURI, isPrivate, scores);
+    }
+
     // External functions that are view
+    function numberOfDAOs() public view returns (uint256) {
+        // TODO: implement
+        return 0;
+    }
+
+    function numberOfTasks(uint256 daoID) public view returns (uint256) {
+        // TODO: implement
+        return 0;
+    }
+
+    function numberOfThreads(
+        uint256 daoID,
+        uint256 taskID
+    ) public view returns (uint256) {
+        // TODO: implement
+        return 0;
+    }
+
+    function taskStatus(
+        uint256 daoID,
+        uint256 taskID
+    ) public view returns (TaskStatus) {
+        // TODO: implement
+        return TaskStatus.TODO;
+    }
+
+    // 特定のDAOの中の活動がaccountが見えるかどうか
+    function canViewDAO(
+        uint256 daoID,
+        address account
+    ) public view returns (bool) {
+        // TODO: implement
+        return false;
+    }
+
+    // 特定のDAOの中のTaskの中の活動がaccountに見えるかどうか
+    function canViewTask(
+        uint256 daoID,
+        uint256 taskID,
+        address account
+    ) public view returns (bool) {
+        // TODO: implement
+        // assignしてから見えるようにするとかすると面白いかも？
+        return false;
+    }
+
+    // XXX: 暗号化できるのは
+    // taskMetadata
+    // threadMetadata
 
     // External functions that are pure
 
@@ -110,6 +334,87 @@ contract TrustX is Initializable, PausableUpgradeable {
         }
 
         emit DAOMembersAdded(daoID, members);
+    }
+
+    function _creataTask(
+        uint256 daoID,
+        string memory name,
+        string memory metadataURI,
+        bool isPrivate,
+        string[] memory skills,
+        address assigner,
+        address[] memory reviewers
+    ) internal {
+        // TODO: implement
+    }
+
+    function _assignAssigner(
+        uint256 daoID,
+        uint256 taskID,
+        address assigner
+    ) internal {
+        // TODO: implement
+        // XXX: task must not be completed
+    }
+
+    function _assignReviewers(
+        uint256 daoID,
+        uint256 taskID,
+        address[] memory reviewers
+    ) internal {
+        // TODO: implement
+        // XXX: task must not be completed
+    }
+
+    function _startTask(uint256 daoID, uint256 taskID) internal {
+        // TODO: implement
+        // XXX: task must not be completed
+        // XXX: must be called from assigner
+    }
+
+    function _addComment(
+        uint256 daoID,
+        uint256 taskID,
+        string memory messageURI,
+        bool isPrivate
+    ) internal {
+        // TODO: implement
+        // XXX: task must not be completed
+        // XXX: must be called from assigner
+    }
+
+    function _requestTaskReview(
+        uint256 daoID,
+        uint256 taskID,
+        string memory messageURI,
+        bool isPrivate
+    ) internal {
+        // TODO: implement
+        // XXX: task must not be completed
+        // XXX: must be called from assigner
+    }
+
+    function _requestChanges(
+        uint256 daoID,
+        uint256 taskID,
+        string memory messageURI,
+        bool isPrivate
+    ) internal {
+        // TODO: implement
+        // XXX: task must not be completed
+        // XXX: must be called from assigner
+    }
+
+    function _approveTask(
+        uint256 daoID,
+        uint256 taskID,
+        string memory messageURI,
+        bool isPrivate,
+        uint256[] memory scores
+    ) internal {
+        // TODO: implement
+        // XXX: task must not be completed
+        // XXX: must be called from assigner
     }
 
     // Private functions
