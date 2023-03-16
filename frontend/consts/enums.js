3a5301e0f22d6732a233e1a56851d0d96b839f50
kourin <kourin.code@gmail.com>
2023-03-16 16:05:26 +0900

    Add constants

---
+++ frontend/consts/enums.js
@@ -0,0 +1,11 @@
+export const TASK_STATUS_TODO = "todo";
+export const TASK_STATUS_IN_PROGRESS = "in_progress";
+export const TASK_STATUS_IN_REVIEW = "in_review";
+export const TASK_STATUS_DONE = "done";
+
+export const TASK_STATUSES = [
+    TASK_STATUS_TODO,
+    TASK_STATUS_IN_PROGRESS,
+    TASK_STATUS_IN_REVIEW,
+    TASK_STATUS_DONE
+]
