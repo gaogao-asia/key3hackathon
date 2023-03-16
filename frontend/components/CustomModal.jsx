f2dd4677a6158348c4400184fc6cb40c7df2df27
hanachan1026 <iwgac1026@gmail.com>
2023-03-16 15:33:26 +0700

    added assignees & reviewers form field

--- frontend/components/CustomModal.jsx
+++ frontend/components/CustomModal.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useState } from "react";
-import { Modal, Form, Input, Button } from 'antd';
+import { Modal, Form, Input, Select, Option, Button } from 'antd';
 
 function CustomModal({ data, visible, onCancel }) {
     const [form] = Form.useForm();
     const [loading, setLoading] = useState(false);
+
+    const users = [
+        { label: 'トヨタ タロウ', value: 'user1' },
+        { label: 'トヨタ ハジメ', value: 'user2' },
+        { label: 'トヨタ ジロウ', value: 'user3' },
+    ];
 
     const handleCancel = () => {
         form.resetFields();
@@ -33,6 +39,28 @@ function CustomModal({ data, visible, onCancel }) {
                     rules={[{ required: true, message: 'Please enter the description' }]}
                 >
                     <Input.TextArea rows={4} />
+                </Form.Item>
+                <Form.Item
+                    label="担当者"
+                    name="assignees"
+                    rules={[{ required: true, message: 'Please select at least one assignee' }]}
+                >
+                    <Select 
+                        mode="multiple" 
+                        placeholder="担当者を選択" 
+                        options={users}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="承認担当者"
+                    name="reviewers"
+                    rules={[{ required: true, message: 'Please select at least one reviewer' }]}
+                >
+                    <Select 
+                        mode="multiple" 
+                        placeholder="承認担当者を選択"
+                        options={users}
+                    />
                 </Form.Item>
             </Form>
         </Modal>
