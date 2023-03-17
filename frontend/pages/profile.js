acddb770475672c2f9065a923bd6be6bb3994fe7
hanachan1026 <iwgac1026@gmail.com>
2023-03-17 22:03:46 +0700

    WIP: Profile Page skill_points

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -8,25 +8,47 @@ import { Card, Avatar, Progress } from 'antd';
 const { Meta } = Card;
 
 export default function Home() {
+    const testData = [
+        { label: "プレゼンテーション", percent: 80 },
+        { label: "技術", percent: 50 },
+        { label: "リーダーシップ", percent: 55 },
+    ];
+
     return (
         <Layout>
             <Card
                 className="flex flex-col items-center justify-center w-1/2 m-8"
-            >
-                <Meta 
-                    className="p-8"
-                    avatar={<Avatar
-                        style={{ backgroundColor: '#87d068' }}
-                        icon={<Image 
-                            src={'/user_01.png'}
-                            width='300'
-                            height='300'
+                title={
+                    <Meta
+                        className="p-8"
+                        avatar={<Avatar
+                            style={{ backgroundColor: '#87d068' }}
+                            icon={<Image
+                                src={'/user_01.png'}
+                                width='300'
+                                height='300'
+                            />}
+                            size={96}
                         />}
-                        size={96}
-                    />}
-                    title="トヨタ タロウ"
-                    description="新規事業部リーダー ソフトウェア開発者"
-                />
+                        title="トヨタ タロウ"
+                        description="新規事業部リーダー ソフトウェア開発者"
+                    />
+                }
+                bodyStyle={{ width: '100%' }}
+            >
+                <div className="w-full">
+                    {testData.map((item, idx) => (
+                        <>
+                            <p>{item.label}</p>
+                            <Progress 
+                                label={item.label} 
+                                percent={item.percent} 
+                                strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
+                                format={(percent) => percent}
+                            />
+                        </>
+                    ))}
+                </div>
             </Card>
         </Layout>
     );
