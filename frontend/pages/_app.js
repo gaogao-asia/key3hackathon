99fb71e34ee05dae3f0fa866a0c1ad3f09dfb8fc
kourin <kourin.code@gmail.com>
2023-03-19 06:53:21 +0900

    Remove hardhat network from FE configurations

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,19 +1,14 @@
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
-import { hardhat, shibuya } from "../consts/chains";
+import { shibuya } from "../consts/chains";
 
 import "./style.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../clients/subquery";
 
-const { chains, provider } = configureChains(
-  [...(process.env.NEXT_PUBLIC_ENV === "dev" ? [hardhat] : []), shibuya],
-  [publicProvider()]
-);
+const { chains, provider } = configureChains([shibuya], [publicProvider()]);
 
 const { connectors } = getDefaultWallets({
   appName: "My RainbowKit App",
