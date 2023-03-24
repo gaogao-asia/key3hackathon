ee4447aafabbee2ec36b3ab8a3ec28815e279082
hanachan1026 <iwgac1026@gmail.com>
2023-03-24 11:22:00 +0700

    WIP: added Badges UI

--- frontend/pages/profiles/[address].js
+++ frontend/pages/profiles/[address].js
@@ -12,9 +12,11 @@ import {
   Typography,
   Descriptions,
   Skeleton,
+  Tag,
 } from "antd";
 import Layout from "../../components/Layout";
 import { Skills } from "../../consts/skills";
+import { Badges } from "../../consts/badges";
 import { useAccountSkills } from "../../hooks/account_skills";
 import { useAccountProfile } from "../../hooks/account_profile";
 import { useTasksByAssigner } from "../../hooks/tasks_by_assigner";
@@ -62,8 +64,28 @@ const TaskCard = (props) => {
   );
 };
 
+const BadgesCard = (props) => {
+  const { badges } = props;
+  return (
+    <Card style={{ width: "100%" }}>
+      <Row gutter={[16, 16]}>
+        {badges.map((id) => (
+          <Col span={6}>
+            <Card
+              style={{ boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)" }}
+              bodyStyle={{ padding: "8px" }}
+            >
+              {Badges[id].name}
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Card>
+  );
+};
+
 export default function Home() {
-  const [doneTasks, setDoneTasks] = useState([])
+  const [doneTasks, setDoneTasks] = useState([]);
   const router = useRouter();
   const { address } = router.query;
   const profile = useAccountProfile(address);
@@ -71,6 +93,8 @@ export default function Home() {
 
   const tasksQuery = useTasksByAssigner(address);
   const skillsQuery = useAccountSkills(address);
+
+  const badgesEarned = [3, 5, 8, 12]; // ToDo: 一旦、ハードコードしている
 
   const skillPoints = useMemo(() => {
     const accountSkills = skillsQuery?.data?.skills ?? [];
@@ -103,18 +127,20 @@ export default function Home() {
   }, [skillsQuery.data]);
 
   useMemo(() => {
-    setDoneTasks((tasksQuery?.data?.tasks ?? [])
-      .slice()
-      .filter((t) => {
-        console.log("debug::t", t);
-        return t.status === "done";
-      })
-      .sort(
-        (a, b) =>
-          Number.parseInt(b.createdBlockHeight) -
-          Number.parseInt(a.createdBlockHeight)
-      ))
-  }, [tasksQuery?.data?.tasks])
+    setDoneTasks(
+      (tasksQuery?.data?.tasks ?? [])
+        .slice()
+        .filter((t) => {
+          console.log("debug::t", t);
+          return t.status === "done";
+        })
+        .sort(
+          (a, b) =>
+            Number.parseInt(b.createdBlockHeight) -
+            Number.parseInt(a.createdBlockHeight)
+        )
+    );
+  }, [tasksQuery?.data?.tasks]);
 
   // hydration errorになる原因わからず、取り急ぎ
   const [firstRender, setFirstRender] = useState(true);
@@ -125,11 +151,11 @@ export default function Home() {
 
   return (
     <Layout>
-      <div 
-        className="flex flex-col items-center" 
-        style={{ 
+      <div
+        className="flex flex-col items-center"
+        style={{
           overflowY: "scroll",
-          zIndex: 0
+          zIndex: 0,
         }}
       >
         <Card
@@ -181,12 +207,20 @@ export default function Home() {
 
         <div className="flex flex-col items-center justify-center w-5/6 m-8">
           <Row gutter={[16, 16]}>
-            { doneTasks.map(task => (
+            {doneTasks.map((task) => (
               <Col span={12} key={task.id}>
                 <TaskCard task={task} />
               </Col>
-            )) }
+            ))}
           </Row>
+        </div>
+
+        <Divider style={{ margin: "0px" }}>
+          <Title level={5}>獲得した称号: {badgesEarned.length}</Title>
+        </Divider>
+
+        <div className="flex flex-col items-center justify-center w-5/6 m-8">
+          <BadgesCard badges={badgesEarned} />
         </div>
       </div>
     </Layout>
